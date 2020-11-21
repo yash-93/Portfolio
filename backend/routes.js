f0b9b81078073cb859ffdcc6d3c9dbf4402e06cb
yash-93 <yashdeepbachhas93@gmail.com>
2020-11-22 03:08:45 +0530

    backend detached

--- backend/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const createProject = require("./controller");
-
-router.post("/createProject", createProject.createProject);
-
-module.exports = router;
