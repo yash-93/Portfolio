fb95e9c108808abc63af6c1ec1a10f69d04fd658
yash-93 <yashdeepbachhas93@gmail.com>
2020-11-24 00:03:31 +0530

    Fetching projects from REST API

--- src/components/ProjectDesc.js
+++ src/components/ProjectDesc.js
@@ -12,26 +12,34 @@ const ProjectDesc = (props) => {
         <img src={proj_pic} alt="thumbnail" />
       </div>
       <div className="project_desc">
-        <h2 id="project_title">Codeforces CLI</h2>
-        <p id="project_content">
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy.
-        </p>
+        <h2 id="project_title">{props.projectdetails.title}</h2>
+        <p id="project_content">{props.projectdetails.description}</p>
         <div className="tech_stack">
-          <Tech tech="Node.js" />
+          {props.projectdetails.techstack.map((tech, index) => (
+            <Tech key={index} tech={tech} />
+          ))}
+          {/* <Tech tech="Node.js" />
           <Tech tech="Puppeteer" />
-          <Tech tech="React-Router" />
+          <Tech tech="React-Router" /> */}
         </div>
         <br></br>
         <div className="code_demo_btns">
-          <Button className="demo">View Demo</Button>
-          <Button className="code">View Code</Button>
+          <Button
+            className="demo"
+            onClick={() => {
+              window.open(`${props.projectdetails.demo}`, "_blank");
+            }}
+          >
+            View Demo
+          </Button>
+          <Button
+            className="code"
+            onClick={() => {
+              window.open(`${props.projectdetails.code}`, "_blank");
+            }}
+          >
+            View Code
+          </Button>
         </div>
       </div>
     </div>
